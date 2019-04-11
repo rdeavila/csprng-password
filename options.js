1ca44d7a7eb84031f5de14e3a0339073429ead3f
Rodrigo de Avila <rodrigo.avila@mailtop.com.br>
2019-04-11 15:59:33 -0300

    feat: add options screen

---
+++ options.js
@@ -0,0 +1,33 @@
+function saveOptions () {
+    var choosenSize = document.getElementById("size").value;
+    var help = document.getElementById("help");
+
+    if (isNaN(parseInt(choosenSize)) || parseInt(choosenSize) < 10 || parseInt(choosenSize) > 50) {
+        help.textContent = "ERROR: number must be integer, and between 10 and 50.";
+    } else {
+        chrome.storage.sync.set({ passwordSize: choosenSize }, function () {
+            help.textContent = "Value saved!";
+            setTimeout(function () {
+                help.textContent = "";
+                window.close();
+            }, 1000);
+        });
+    }
+}
+
+function restoreOptions () {
+    chrome.storage.sync.get(["passwordSize"], function (result) {
+        if (result.passwordSize == undefined) {
+            document.getElementById("size").value = "10";
+        } else {
+            document.getElementById("size").value = result.passwordSize;
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    restoreOptions();
+});
+document.getElementById("save").addEventListener("click", function () {
+    saveOptions();
+});
--- popup.js
+++ popup.js
@@ -1,16 +1,27 @@
 function getPassword () {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://csprng.xyz/v1/api?length=25", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            var resp = JSON.parse(xhr.responseText);
-            var pwd = resp.Data.replace(/[^\w\s]/gi, '')
+    chrome.storage.sync.get(["passwordSize"], function (result) {
+        var passwordSize = 10;
 
-            document.getElementById("password").innerHTML = pwd
-            document.getElementById("date").innerHTML = pwd.length + " characters generated at " + resp.Time
+        if (result.passwordSize == undefined) {
+            passwordSize = 10;
+        } else {
+            passwordSize = parseInt(result.passwordSize);
         }
-    }
-    xhr.send();
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://csprng.xyz/v1/api?length=55", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                var resp = JSON.parse(xhr.responseText);
+                var pwd = resp.Data.replace(/[^\w\s]/gi, '').substring(0, passwordSize);
+
+                document.getElementById("password").innerHTML = pwd;
+                document.getElementById("date").innerHTML = pwd.length + " characters generated at " + resp.Time
+            }
+        }
+        xhr.send();
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
